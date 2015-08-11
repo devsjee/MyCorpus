bd61842062b13d8170d1ff42db0d62937f617228
devsjee <devsjee@gmail.com>
2015-08-12 01:45:16 +0530

    oops

--- myCorpus.py
+++ myCorpus.py
@@ -7,7 +7,7 @@ VOCAB ={}
 
 #fnames is the the total list of files from which input is processed
 fnames =['f1.txt']
-N = 4
+N = 1
 
 #corpus is the string form of all text files processed so far
 CORPUS =""
@@ -83,7 +83,7 @@ def plot_vocab_hist(fname):
 	#bins =[1,50,100,150,200,250,300,400,500,1000,1500,2000]
 	#n,bins,patches = pylab.hist(freq,bins,normed=0,histtype='bar')
 
-	pylab.hist(freq)
+	pylab.hist(freq,50)
 	pylab.xlabel('frequency')
 	pylab.ylabel('frequency of frequency')
 	pylab.title('Corpus frequency distribution after adding '+fname)
@@ -105,7 +105,7 @@ def print_analysis():
 
 def main():
 	for i in range(1,N+1):
-		fname = "f"+str(i)+".txt"
+		fname = "check"+str(i)+".txt"
 		process_text(fname)
 		add_to_corpus(fname)
 		plot_vocab_hist(fname)		
